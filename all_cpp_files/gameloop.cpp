ae6f2bc24d27d6128814ed33c5422d6f17c30adb
itsDipsy <paregos55.collins@gmail.com>
2023-01-14 20:19:13 +0100

    Update gameloop.cpp

--- all_cpp_files/gameloop.cpp
+++ all_cpp_files/gameloop.cpp
@@ -4,15 +4,6 @@ void the_game_loop(int player, int gamemap[4][10]){
     bool the_game_running_value = true;
     while (the_game_running_value)
     {
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-        for(int i = 0; i < 4; i++){
-            std::cout << gamemap[i] << std::endl;
-            if(i == 4){
-                the_game_running_value = false;
-=======
-=======
->>>>>>> Stashed changes
         for(int i = 0; i <= 4; i++){
             for (int j = 0; j <= 10; j++)
             {
