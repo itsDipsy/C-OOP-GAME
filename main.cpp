cfcd3431d88af994069cfef68d0b0a6b79a0675b
itsDipsy <paregos55.collins@gmail.com>
2023-01-14 17:09:48 +0100

    project restart

--- main.cpp
+++ main.cpp
@@ -1,21 +1,6 @@
 #include <iostream>
-#include "./headers/player.hpp"
-#include "./headers/TheGameMap.hpp"
+
 int main()
 {
-    bool running = true;
-    player Theplayer;
-    int theRealPlayer = Theplayer.createPlayer(running);
-    if (running == true)
-    {
-        gameMap[0][0] = theRealPlayer;
-    }
-    for (int i = 0; i < 10; i++)
-    {
-        std::cout << std::endl;
-        for (int i = 0; i < 10; i++)
-        {
-            std::cout << gameMap[i][i];
-        }
-    }
+    printf("hello world");
 }
