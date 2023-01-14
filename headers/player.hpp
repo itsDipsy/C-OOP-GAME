d434043a27431204134ff2d8dc60bb7db9ad91b8
itsDipsy <paregos55.collins@gmail.com>
2023-01-14 18:07:22 +0100

    uppdate

--- headers/player.hpp
+++ headers/player.hpp
@@ -1,3 +1,3 @@
 
-int player; // this is the player variable which is gonna be a one for the mean while anyways, its gonna be a ascii type game
+int player = 1; // this is the player variable which is gonna be a one for the mean while anyways, its gonna be a ascii type game
 
